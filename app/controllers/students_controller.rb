de58dbc66c9ef8ed8849e41165270436be05ac55
Wyndell Bibera <wmbibera@up.edu.ph>
2017-06-12 12:54:40 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,17 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    @student.active ? @student.update_column(:active, false) : @student.update_column(:active, true)
+    redirect_to action: "show"
   end
 
   private
