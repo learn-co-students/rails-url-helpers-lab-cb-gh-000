7b4f3267ebba2dc3d305f3ab247bb5e89e554b7e
Saujan Ghimire <supertrampsookah@gmail.com>
2017-09-05 14:57:04 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,20 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    respond_to do |format|
+      format.html { redirect_to student_path(@student), notice: 'Your status has been changed!.' }
+    end
   end
 
   private
