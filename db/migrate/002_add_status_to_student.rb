3677b742cd75e27873c06c6f36241a25761f5547
Othman Mashaab <red.fantastic@hotmail.com>
2018-02-19 17:14:26 +0000

    Done.

---
+++ db/migrate/002_add_status_to_student.rb
@@ -0,0 +1,5 @@
+class AddStatusToStudent < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 20180219163439) do
 
-  create_table "students", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "students" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
 end
